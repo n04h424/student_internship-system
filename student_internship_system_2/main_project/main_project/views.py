6061494dd3bb74c23e5637c0aa7a63b9ad308ee7
n04h424 <143258261+n04h424@users.noreply.github.com>
2026-05-10 19:48:24 +0400

    Add files via upload

--- student_internship_system_2/main_project/main_project/views.py
+++ student_internship_system_2/main_project/main_project/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Home")
+    return render(request,'homepage.html')
+    # when this function is called, go back one level, look for templates, and render 'homepage.html'
 
 def about(request):
-    return HttpResponse("About")
+    return render(request,'about.html')
+    # when this function is called, go back one level, look for templates, and render 'about.html'
